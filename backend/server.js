a8a9c3cbaa21f0bee9fedf87c4e6c80a7bcaee40
Ruthwik-3556 <cs23bt056@iitdh.ac.in>
2026-09-06 13:05:16 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ pool.query("SELECT NOW()", (err) => {
 });
 
 // Start server
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running");
 });
